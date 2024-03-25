2677aab5451aa682d4c8d07f2fc1c0524a7b7aad
AmdadulHQ <rimonamdadul301@gmail.com>
2024-03-25 23:44:01 +0600

    adding book details section

--- src/Pages/BookDetails/BookDetails.jsx
+++ src/Pages/BookDetails/BookDetails.jsx
@@ -1,15 +1,33 @@
-import { useLoaderData } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const BookDetails = () => {
+    const [showBook ,setShowBook] = useState([])
+    console.log(showBook);
+    const {bookId,image,bookName,author,review,totalPages,rating,category,tags,publisher,yerOfPublishing} = showBook
     const books = useLoaderData()
-    console.log(books);
-
+    const {id} = useParams()
+    useEffect(() =>{
+        const book = books.find( bk => bk.bookId == id)
+        setShowBook(book)
+    },[])
     return (
         <section>
             <div>
+                <img src={image} alt="" />
+            </div>
+            <div>
+                <h1>{bookName}</h1>
+                <p>By : {author}</p>
+                <p>{category}</p>
+                <p><strong>Review :</strong>{review}</p>
+                <div className="flex gap-x-3">
+                {
+                    tags.map( tag => <p className="text-[#23BE0A] text-base font-medium px-4 py-2 rounded-[30px] bg-[rgba(35,190,10,0.05)]" key={bookId}>{tag}</p> )
+                }
+                </div>
 
             </div>
-            <div></div>
         </section>
     );
 };
