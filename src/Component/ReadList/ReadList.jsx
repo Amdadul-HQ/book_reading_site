590395131c7eb89fb503d5e991dcfed13672c3a0
AmdadulHQ <rimonamdadul301@gmail.com>
2024-03-26 13:57:32 +0600

    filter feature added in wish list and read list

--- src/Component/ReadList/ReadList.jsx
+++ src/Component/ReadList/ReadList.jsx
@@ -1,16 +1,58 @@
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { getLocalStorage } from "../../utility/SetToLocalStorage";
 import ReadBook from "../ReadBook/ReadBook";
+import { useEffect, useState } from "react";
 
 
 const ReadList = () => {
     const books = useLoaderData()
-    const readBook = getLocalStorage()
-    const showReadBook = books.filter( book => readBook.includes(book.bookId))
+    const [showBooks,setShowBooks] = useState([])
+    const [display,setDisplay] = useState([])
+
+    const handlefilter = text => {
+        if(text == 'sortby'){
+            setDisplay(showBooks)
+        }
+        else if( text == 'rating' ) {
+            setDisplay( showBooks.sort((a,b)=> b.rating - a.rating))
+          }
+        else if( text == 'pages' ){
+            setDisplay(showBooks.sort( (a,b) => b.totalPages - a.totalPages ))
+        }
+        else if( text == 'publish' ){
+            setDisplay(showBooks.sort( (a,b) => b.yearOfPublishing - a.yearOfPublishing ))
+        }
+    }
+    
+  
+
+
+    useEffect(() => {
+        const readBook = getLocalStorage()
+
+        if(books.length){
+            const showReadBook = books.filter( book => readBook.includes(book.bookId))
+            setShowBooks(showReadBook)
+            setDisplay(showReadBook)
+        }
+    },[])
+
+
     return (
         <div className="space-y-6">
+            <div className="flex justify-center my-5">
+			<details className="dropdown">
+			<summary className="m-1 btn bg-[#23BE0A] text-lg font-semibold text-white hover:text-[#131313] mx-auto">Sort By</summary>
+			<ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+				<Link to='' onClick={() => handlefilter('sortby')}><a>Sort By</a></Link>
+				<Link to='' onClick={() => handlefilter('rating')}><a>Rating</a></Link>
+				<Link to='' onClick={() => handlefilter('pages')}><a>Number Of Pages</a></Link>
+				<Link to='' onClick={() => handlefilter('publish')}><a>Published Year</a></Link>
+			</ul>
+			</details>
+			</div>
             {
-                showReadBook.map( book => <ReadBook key={book.bookId} book={book}></ReadBook> )
+                display.map( book => <ReadBook key={book.bookId} book={book}></ReadBook> )
             }
         </div>
     );
--- src/Component/Wishlist/Wishlist.jsx
+++ src/Component/Wishlist/Wishlist.jsx
@@ -1,17 +1,56 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useLoaderData } from 'react-router-dom';
 import { getWishListedId } from '../../utility/SetToLocalStorage';
 import Whitels from '../Whitels/Whitels';
 
 const Wishlist = () => {
     const books = useLoaderData()
-    const wishlist = getWishListedId()
-    const showWishlist = books.filter( book => wishlist.includes(book.bookId) )
+    const [showBooks,setShowBooks] = useState([])
+    const [display,setDisplay] = useState([])
+
+    const handlefilter = text => {
+        if(text == 'sortby'){
+            setDisplay(showBooks)
+        }
+        else if( text == 'rating' ) {
+            setDisplay( showBooks.sort((a,b)=> b.rating - a.rating))
+          }
+        else if( text == 'pages' ){
+            setDisplay(showBooks.sort( (a,b) => b.totalPages - a.totalPages ))
+        }
+        else if( text == 'publish' ){
+            setDisplay(showBooks.sort( (a,b) => b.yearOfPublishing - a.yearOfPublishing ))
+        }
+    }
+    
+  
+
+
+    useEffect(() => {
+        const wishBook = getWishListedId()
+
+        if(books.length){
+            const showWishBook = books.filter( book => wishBook.includes(book.bookId))
+            setShowBooks(showWishBook)
+            setDisplay(showWishBook)
+        }
+    },[])
     
     return (
         <div className="space-y-6">
+              <div className="flex justify-center my-5">
+			<details className="dropdown">
+			<summary className="m-1 btn bg-[#23BE0A] text-lg font-semibold text-white hover:text-[#131313] mx-auto">Sort By</summary>
+			<ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+				<Link to='' onClick={() => handlefilter('sortby')}><a>Sort By</a></Link>
+				<Link to='' onClick={() => handlefilter('rating')}><a>Rating</a></Link>
+				<Link to='' onClick={() => handlefilter('pages')}><a>Number Of Pages</a></Link>
+				<Link to='' onClick={() => handlefilter('publish')}><a>Published Year</a></Link>
+			</ul>
+			</details>
+			</div>
             {
-                showWishlist.map( book => <Whitels key={book.bookId} book={book}></Whitels> )
+                display.map( book => <Whitels key={book.bookId} book={book}></Whitels> )
             }
         </div>
     );
